5dcee61ea5e10c0afe15954636625d75ee54ec55
Pedrodevlatam <pamp1891@gmail.com>
2023-05-07 01:28:46 -0500

    C# Prep 3—Loops Update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,53 @@
 using System;
 
-class Program
+using System;
+
+class GuessMyNumberGame
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Welcome message
+        Console.WriteLine("Welcome to the Guess My Number game!");
+
+        while (true)
+        {
+            // Generate a random number from 1 to 100
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int numGuesses = 0;
+
+            // Game loop
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                numGuesses++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it in {0} guesses!", numGuesses);
+                    break;
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgain = Console.ReadLine();
+
+            if (playAgain.ToLower() != "yes")
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
