e11ece53dabd27f5eaaf570459c20dfdf4470d9c
Pedrodevlatam <pamp1891@gmail.com>
2023-07-06 23:06:18 -0500

    Update_week09

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        // Create instances of Square, Rectangle, and Circle and add them to the list
+        Square square = new Square("Red", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 3);
+        shapes.Add(circle);
+
+        // Iterate through the list of shapes and display their color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: " + shape.Color);
+            Console.WriteLine("Area: " + shape.GetArea());
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
     }
 }
